1624e3c1b70068a8b7192e977c4d988fe1d2c2c2
Monthon Intraraprasit <monthonintraraprasit@Monthons-MacBook-Air.local>
2016-10-12 16:49:53 +0700

    edit queen class

--- src/Queen.java
+++ src/Queen.java
@@ -2,11 +2,31 @@
  * Created by monthonintraraprasit on 10/12/2016 AD.
  */
 public class Queen {
-    int numberOfQueen;
+    private int numberOfQueen;
+    public int position_x;
+    public int position_y;
     public Queen(int numberOfQueen){
         this.numberOfQueen = numberOfQueen;
+    }
+    public Queen(int numberOfQueen,int position_x,int position_y){
+        this.numberOfQueen = numberOfQueen;
+        this.position_x = position_x;
+        this.position_y = position_y;
     }
     public int getnumberOfQueen(){
         return numberOfQueen;
     }
+    public int getPosition_X(){
+        return position_x;
+    }
+    public int getPosition_y(){
+        return position_y;
+    }
+    public void setPosition_x(int position_x){
+        this.position_x = position_x;
+    }
+    public void setPosition_y(int position_y){
+        this.position_y = position_y;
+    }
+
 }
--- src/View.java
+++ src/View.java
@@ -3,19 +3,18 @@
  */
 public class View {
     private char[][] table = new char[8][8];
-    public int[] position_x = new int[8];
-    public int[] position_y = new int[8];
+    public Queen[] queen = new Queen[8];
     public View(char[][] table){
         this.table = table;
         int countQueeen = 0;
-        for(int i=0;i<table.length;i++){
-            for(int j=0;j<table[i].length;j++){
-                if(table[i][j]=='q'){
-                    this.position_x[countQueeen] = i;
-                    this.position_y[countQueeen] = j;
-                    countQueeen += 1;
-                }
-            }
+//        for(int i=0;i<table.length;i++){
+//            for(int j=0;j<table[i].length;j++){
+//                if(table[i][j]=='q'){
+//                    this.position_x[countQueeen] = i;
+//                    this.position_y[countQueeen] = j;
+//                    countQueeen += 1;
+//                }
+//            }
         }
     }
     public View(View view){
@@ -23,33 +22,21 @@ public class View {
             this.table[i]= view.getTable()[i].clone();
         }
         int countQueeen = 0;
-        for(int i=0;i<view.getTable().length;i++){
-            for(int j=0;j<view.getTable()[i].length;j++){
-                if(table[i][j]=='q'){
-                    this.position_x[countQueeen] = i;
-                    this.position_y[countQueeen] = j;
-                    countQueeen += 1;
-                }
-            }
-        }
+//        for(int i=0;i<view.getTable().length;i++){
+//            for(int j=0;j<view.getTable()[i].length;j++){
+//                if(table[i][j]=='q'){
+//                    this.position_x[countQueeen] = i;
+//                    this.position_y[countQueeen] = j;
+//                    countQueeen += 1;
+//                }
+//            }
+//        }
     }
     public char[][] getTable(){
         return table;
     }
     public void setTable(char[][] table){
         this.table = table;
-    }
-    public int[] getQueenPosition_X(){
-        return position_x;
-    }
-    public int[] getQueenPosition_y(){
-        return position_y;
-    }
-    public void setQueenPosition_x(int numberofqueen,int position_x){
-        this.getQueenPosition_X()[numberofqueen] = position_x;
-    }
-    public void setQueenPosition_y(int numberofqueen,int position_y){
-        this.getQueenPosition_y()[numberofqueen] = position_y;
     }
     private void setValueInitial(){
         this.table = new char[][]{  {' ',' ',' ',' ',' ',' ',' ',' '},
